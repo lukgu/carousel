975bcdf82a48f4792830bff039ff3cd8dca3f2b5
lukgu <szukasz.gutowski@gmail.com>
2016-07-31 18:12:26 +0200

    carousel-2

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
 var carouselList = $("#carousel ul");
-var changeSlide = carouselList.animate({'marginLeft':-500}, 500, moveFirstSlide);
+var sumMargin = 0;
+var changeSlide = function() {
+sumMargin =500;
+console.log(sumMargin);
+carouselList.animate({'marginLeft':-sumMargin}, 500, moveFirstSlide);
+}
+function moveFirstSlide(){
+var firstItem = carouselList.find("li:first");
+var lastItem = carouselList.find("li:last");
+$(lastItem.after(firstItem));
+$(carouselList.css({marginLeft:0}));
 
+};
 
 $(function(){
-	$('carouselList').setInterval(changeSlide,3000);
+setInterval(changeSlide, 3000);
+
+
+});
+
+
+
+
+
+
+/*var carouselList = $("#carousel ul");
+var changeSlide = carouselList.animate({'marginLeft':-500}, 500, moveFirstSlide);
+function moveFirstSlide(){
+	var firstItem = carouselList.find("li:first");
+	var lastItem = carouselList.find("li:last");
+	$(lastItem.after(firstItem));
+	$(carouselList.css({marginLeft:0}));
+
+};
+
+$(function(){
+	setInterval(changeSlide, 3000);
 	
 
 });
 
 
+*/
